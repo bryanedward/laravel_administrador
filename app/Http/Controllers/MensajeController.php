4591ebd0dce97b18ce1d25355dcfc71d9773e92c
Bryan Edward Pomaquero Quimiz <edwardbrian96@gmail.com>
2020-07-02 11:37:00 -0500

    quitar use app user del controlador message

--- app/Http/Controllers/MensajeController.php
+++ app/Http/Controllers/MensajeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Mensajes;
-use AA\User;
 use App\Events\MessageWasReceived;
 use App\Http\Requests\validarFormulario;
 use Illuminate\Support\Facades\Cache;
